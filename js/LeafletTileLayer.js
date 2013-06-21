d5bcf9e0447a0750ee0114bc24b232b89a25f909
Brandon Morrison <brandon@brandonmorrison.com>
2013-06-21 17:43:33 -0400

    Cleanup of master branch. Added blank readme and spec file

--- js/LeafletTileLayer.js
+++
@@ -1,9 +0,0 @@
-function LeafletTileLayer() {
-
-}
-
-LeafletTileLayer.prototype.render = function(mapObject, configOptions) {
-  L.tileLayer(configOptions.tileURI, {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(mapObject);
-}
--- js/main.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  var mapJSONEngine = new MapJSON();
-  $.getJSON('js/mapconfig.json', function(mapConfig) {
-    $.getJSON('js/map.json', function(mapData) {
-      mapJSONEngine.loadConfig(mapConfig);
-      mapJSONEngine.render('map', mapData);
-    });
-  });
-})(jQuery);
